a61c226aab817c2714b1e70fde5c8701ef926cf3
xianii <xianyi.xia@outlook.com>
2024-02-21 22:20:43 +0800

    add website

---
+++ website/src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+
+<nav>
+	<a href="/">新老黄历</a>
+	<a href="/templates" data-sveltekit-prefetch>模板词条</a>
+</nav>
+<slot />
+
+<style>
+	nav {
+		display: flex;
+	}
+	nav a {
+		margin-right: 1rem;
+	}
+</style>
---
+++ website/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+
+<script>
+	export let data;
+	const caches = data;
+</script>
+
+
+<h2>今日指引</h2>
+<h2>我的老黄历</h2>
+
+<h2>别人的老黄历</h2>
+
+<p>{caches.date}</p>
+{#each Object.entries(caches.caches) as [name, content]}
+<p>{content.name}:{content.result}</p>
+{/each}
