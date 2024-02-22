1260560bb26014af6fb91caef7b5e9babe7bb070
xianii <xianyi.xia@outlook.com>
2024-02-22 14:48:05 +0800

    website update

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -3,47 +3,41 @@
 	const caches = data;
 </script>
 
-<div class="flex flex-col">
-	<div class="select-none text-lg w-full text-center font-bold mt-4">今日指引</div>
-	<div class="stats stats-horizontal shadow">
-		<div class="stat text-center">
-			<div class="stat-title">穿搭</div>
-			<div class="stat-value">-</div>
-			<div class="stat-desc">Coming Soon</div>
+<div class="flex justify-center w-full">
+	<div class="flex flex-col max-w-lg lg:max-w-3xl">
+		<div class="select-none text-xl lg:text-3xl w-full text-center font-bold mt-4">今日指引</div>
+		<div class="stats stats-horizontal shadow">
+			<div class="stat text-center">
+				<div class="stat-title">穿搭</div>
+				<div class="stat-value">-</div>
+				<div class="stat-desc">Coming Soon</div>
+			</div>
+
+			<div class="stat text-center">
+				<div class="stat-title">饮食</div>
+				<div class="stat-value">-</div>
+				<div class="stat-desc">Coming Soon</div>
+			</div>
+
+			<div class="stat text-center">
+				<div class="stat-title">出行</div>
+				<div class="stat-value">-</div>
+				<div class="stat-desc">Coming Soon</div>
+			</div>
 		</div>
 
-		<div class="stat text-center">
-			<div class="stat-title">饮食</div>
-			<div class="stat-value">-</div>
-			<div class="stat-desc">Coming Soon</div>
-		</div>
-
-		<div class="stat text-center">
-			<div class="stat-title">出行</div>
-			<div class="stat-value">-</div>
-			<div class="stat-desc">Coming Soon</div>
+		<div class="select-none text-xl lg:text-3xl w-full text-center font-bold mt-4">众生的老黄历</div>
+		<div class="overflow-x-auto">
+			<table class="table table-sm table-zebra">
+				<tbody>
+					{#each Object.entries(caches.caches) as [name, content]}
+						<tr>
+							<td>{content.name}</td>
+							<td>{content.result}</td>
+						</tr>
+					{/each}
+				</tbody>
+			</table>
 		</div>
 	</div>
-</div>
-<div class="select-none self-start text-xl w-full text-center font-bold mt-4">我的老黄历</div>
-<div class="text-center">Coming Soon...</div>
-
-<div class="select-none self-start text-xl w-full text-center font-bold mt-4">众生的老黄历</div>
-<div class="overflow-x-auto">
-	<table class="table table-zebra">
-		<thead>
-			<tr>
-				<th>{caches.date}</th>
-				<th>老黄历</th>
-			</tr>
-		</thead>
-		<tbody>
-			{#each Object.entries(caches.caches) as [name, content]}
-				<tr>
-					<td>{content.name}</td>
-					<td>{content.result}</td>
-				</tr>
-			{/each}
-		</tbody>
-	</table>
 </div>
--- website/src/routes/templates/+page.svelte
+++ website/src/routes/templates/+page.svelte
@@ -3,17 +3,53 @@
 	const templates = data;
 </script>
 
-{#each Object.entries(templates) as [name, content]}
-<div class="collapse collapse-arrow border border-base-300 bg-base-200 mt-1">
-	<input type="checkbox" /> 
-	<div class="collapse-title text-lg font-bold">
-		<div class="badge badge-accent">{'{{'}{name}{'}}'}</div>
-		<div class="text-sm">{content.desc}</div>
-	</div>
-	<div class="collapse-content">
-		{#each content.values as v}
-		<div class="badge badge-secondary badge-outline m-1">{v}</div>
-		{/each}
+<div class="flex justify-center w-full">
+	<div class="flex flex-col max-w-lg lg:max-w-3xl">
+		<div class="select-none text-xl lg:text-3xl w-full text-center font-bold mt-4">模板提名</div>
+		<div class="flex flex-row gap-2 mt-1">
+			<input
+				type="text"
+				placeholder="输入含模板的词条"
+				class="grow input input-bordered input-primary"
+			/>
+			<button class="btn btn-primary">测试词条</button>
+		</div>
+		<div class="select-none text-xl lg:text-3xl w-full text-center font-bold mt-4">模板列表</div>
+		<div class="flex flex-row justify-center flex-wrap gap-2 max-w-max">
+			{#each Object.entries(templates) as [name, content]}
+				<div
+					class="flex-none has-tooltip min-w-min border rounded-full border-primary overflow-hidden"
+				>
+					<div class="mt-12 tooltip min-w-min max-w-lg rounded-xl p-1 bg-primary flex flex-row justify-center flex-wrap gap-1">
+						{#each content.values as v}
+							<div class="border border-neutral rounded-md text-neutral text-sm pl-1 pr-1">{v}</div>
+						{/each}
+					</div>
+					<div class="bg-primary text-neutral w-full text-md font-bold pl-2 pr-2">{'{{'}{name}{'}}'}</div>
+					<div class="text-sm text-center pl-2 pr-2">{content.desc}</div>
+					<!-- <div class="collapse-content">
+						{#each content.values as v}
+							<div class="badge badge-secondary badge-outline m-1">{v}</div>
+						{/each}
+					</div> -->
+				</div>
+			{/each}
+		</div>
+		<!-- <div class="grid lg:grid-cols-2 gap-4 max-w-lg lg:max-w-3xl">
+			{#each Object.entries(templates) as [name, content]}
+				<div class="collapse collapse-arrow border border-base-300 bg-base-200 mt-1">
+					<input type="checkbox" />
+					<div class="collapse-title text-lg font-bold">
+						<div class="badge badge-accent">{'{{'}{name}{'}}'}</div>
+						<div class="text-sm">{content.desc}</div>
+					</div>
+					<div class="collapse-content">
+						{#each content.values as v}
+							<div class="badge badge-secondary badge-outline m-1">{v}</div>
+						{/each}
+					</div>
+				</div>
+			{/each}
+		</div> -->
 	</div>
 </div>
-{/each}
